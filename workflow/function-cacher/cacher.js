129b9b7e72d719037f64dcfa1168e1461926855f
Ansagan-B <ansagan.gb@gmail.com>
2021-08-12 20:23:06 +0300

    fix: correct cache function

--- workflow/function-cacher/cacher.js
+++ workflow/function-cacher/cacher.js
@@ -2,21 +2,23 @@ const input = document.getElementById('input');
 const calc = document.getElementById('calc');
 const result = document.getElementById('result');
 
-
 class Cacher {
-    constructor() {
-        this.cache = new WeakMap();
-    }
-
-    withCache(obj) {
-        if (!this.cache.has(obj)) {
-            this.cache.set(obj, obj);
+    withCache(fn) {
+        let cache = {};
+        return function (...args) {
+            const key = args.join()
+            if (cache[key]) {
+                return cache[key];
+            } else {
+                cache[key] = fn(key);
+                return cache[key];
+            }
         }
-        return this.cache.get(obj);
     }
 }
 
 function factorial(number) {
+    console.log(`Number: ${number}`);
     return math.factorial(number);
 }
 
@@ -24,5 +26,6 @@ const cacher = new Cacher();
 const cachedFactorial = cacher.withCache(factorial)
 
 calc.addEventListener('click', () => {
+    console.log(cachedFactorial(input.value))
     result.textContent = cachedFactorial(input.value);
 });
