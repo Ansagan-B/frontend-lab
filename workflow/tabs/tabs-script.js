029ccec3d892ed646b3417efa7ef5ee4a6396d4a
Ansagan-B <ansagan.gb@gmail.com>
2021-08-19 09:39:12 +0300

    fix: coding convention

--- workflow/tabs/tabs-script.js
+++ workflow/tabs/tabs-script.js
@@ -2,26 +2,30 @@ const tabs = [
     {
         id: 1,
         title: 'Javascript',
-        content: "JavaScript — прототипно-ориентированный сценарный язык программирования. Является реализацией языка ECMAScript. " + `<br><br>` +
-            "JavaScript обычно используется как встраиваемый язык для программного доступа к объектам приложений. Наиболее широкое применение находит в браузерах как язык сценариев для придания интерактивности веб-страницам.",
+        content: 'JavaScript — прототипно-ориентированный сценарный язык программирования. Является реализацией языка ECMAScript. ' + `<br><br>` +
+            'JavaScript обычно используется как встраиваемый язык для программного доступа к объектам приложений. ' +
+            'Наиболее широкое применение находит в браузерах как язык сценариев для придания интерактивности веб-страницам.',
         tabIndex: 'tabContent1'
     },
     {
         id: 2,
         title: 'Jquery',
-        content: "jQuery — библиотека JavaScript, фокусирующаяся на взаимодействии JavaScript и HTML. Библиотека jQuery помогает легко получать доступ к любому элементу DOM, обращаться к атрибутам и содержимому элементов DOM, манипулировать ими. Также библиотека jQuery предоставляет удобный API для работы с AJAX. Сейчас разработка jQuery ведётся командой jQuery во главе с Джоном Резигом.",
+        content: 'jQuery — библиотека JavaScript, фокусирующаяся на взаимодействии JavaScript и HTML. ' +
+            'Библиотека jQuery помогает легко получать доступ к любому элементу DOM, обращаться к атрибутам и содержимому элементов DOM, манипулировать ими. ' +
+            'Также библиотека jQuery предоставляет удобный API для работы с AJAX. Сейчас разработка jQuery ведётся командой jQuery во главе с Джоном Резигом.',
         tabIndex: 'tabContent2'
     },
     {
         id: 3,
         title: 'DOM',
-        content: "DOM — это не зависящий от платформы и языка программный интерфейс, позволяющий программам и скриптам получить доступ к содержимому HTML-, XHTML- и XML-документов, а также изменять содержимое, структуру и оформление таких документов.",
+        content: 'DOM — это не зависящий от платформы и языка программный интерфейс, позволяющий программам и скриптам получить доступ к содержимому ' +
+            'HTML-, XHTML- и XML-документов, а также изменять содержимое, структуру и оформление таких документов.',
         tabIndex: 'tabContent3'
     },
     {
         id: 4,
-        title: "CSS",
-        content: "CSS — формальный язык описания внешнего вида документа, написанного с использованием языка разметки.",
+        title: 'CSS',
+        content: 'CSS — формальный язык описания внешнего вида документа, написанного с использованием языка разметки.',
         tabIndex: 'tabContent4'
     }
 ];
@@ -31,8 +35,8 @@ const tabsContent = document.getElementById('tabsContent');
 
 tabsNav.innerHTML = '';
 tabs.forEach(tab => {
-    const navs = `<li class="tab-links" id="${tab.id}" onclick="selectTab(${tab.id})">${tab.title}</li>`
-    const content = `<div class="tabs-content" id="${tab.tabIndex}">
+    const navs = `<li class='tab-links' id='${tab.id}' onclick='selectTab(${tab.id})'>${tab.title}</li>`
+    const content = `<div class='tabs-content' id='${tab.tabIndex}'>
                         <p>${tab.content}</p>
                      </div>`
     tabsNav.innerHTML += navs;
@@ -47,15 +51,17 @@ function toArray(obj) {
     if (Object.prototype.toString.call(obj) === '[object Array]') {
         return obj;
     }
+
     let objectsArray = [];
+
     for (let i = 0; i < obj.length; i++) {
         objectsArray.push(obj[i]);
     }
+
     return objectsArray;
 }
 
 function selectTab(index = 1) {
-
     const selected = document.getElementsByClassName('selected');
 
     toArray(selected).forEach(item => {
