d2775c843ce04f4f459a7e1178987d1e0074dc9d
Ansagan-B <ansagan.gb@gmail.com>
2021-08-25 11:50:41 +0300

    fix: Added differentiation by type

--- workflow/json-visualizer/script.js
+++ workflow/json-visualizer/script.js
@@ -1,8 +1,16 @@
-const textarea = document.getElementById('text-area').value;
+const textarea = document.getElementById('text-area');
 const result = document.getElementById('result');
 const button = document.getElementById('button');
+const clearButton = document.getElementById('clear-button');
 
 button.addEventListener('click', showResult);
+clearButton.addEventListener('click', clearTextarea);
+
+function clearTextarea() {
+    textarea.value = '';
+    result.textContent = '';
+    result.innerHTML;
+}
 
 function parseJSONString(str) {
     try {
@@ -13,19 +21,51 @@ function parseJSONString(str) {
 }
 
 function showResult() {
-    let parsedJSON = parseJSONString(textarea)
+    let val = textarea.value;
+    let parsedJSON = parseJSONString(val)
+    let resultArray = [];
 
     if (parsedJSON === null) {
         result.textContent = 'Wrong format';
+        result.innerHTML;
     } else {
 
-        for (let key in parsedJSON){
-            JSON.stringify(key);
-            result.textContent += `${key}, `;
-        }
+        for (let [key, value] of Object.entries(parsedJSON)) {
 
+            if (typeof value === 'object') {
+                resultArray.push(
+                    `<span">${key}{${Object.keys(value).length}}:</span>`
+                );
+                resultArray.push(jsonToHtml(value));
+            } else if (Array.isArray(value)) {
+                resultArray.push(`<span>${key} [${value.length}]:`);
+                resultArray.push(jsonToHtml(value));
+            } else {
+                let content = value;
+
+                if (typeof content === 'number') {
+                    resultArray.push(
+                        `<p>${key}: 
+                        <span class='blue'>${content}</span>
+                    </p>`
+                    );
+                } else if (typeof content === 'string') {
+                    resultArray.push(
+                        `<p>${key}: 
+                        <span class='red'>${content}</span>
+                    </p>`
+                    );
+                } else {
+                    resultArray.push(
+                        `<p>${key}: 
+                        <span class='green'>${content}</span>
+                    </p>`
+                    );
+                }
+            }
+        }
+        return result.innerHTML = resultArray.join('');
     }
-    result.innerHTML;
 }
 
 
