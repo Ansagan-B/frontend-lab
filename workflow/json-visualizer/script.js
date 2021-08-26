18e09273584af48589677162ceddef81cbcc4b1c
Ansagan-B <ansagan.gb@gmail.com>
2021-08-26 15:31:09 +0300

    fix: refactor adding classes

--- workflow/json-visualizer/script.js
+++ workflow/json-visualizer/script.js
@@ -3,7 +3,7 @@ const result = document.getElementById('result');
 const button = document.getElementById('button');
 const clearButton = document.getElementById('clear-button');
 
-button.addEventListener('click', showResult);
+button.addEventListener('click', () => renderResult(textarea.value));
 clearButton.addEventListener('click', clearTextarea);
 
 function clearTextarea() {
@@ -20,8 +20,19 @@ function parseJSONString(str) {
     }
 }
 
-function showResult() {
-    let val = textarea.value;
+function addClassByType(content) {
+    if (typeof content === 'number') {
+        return 'blue';
+    } else if (typeof content === 'string') {
+        return 'red';
+    } else if (typeof content === 'boolean') {
+        return 'gray';
+    } else {
+        return 'green';
+    }
+}
+
+function showResult(val) {
     let parsedJSON = parseJSONString(val)
     let resultArray = [];
 
@@ -33,39 +44,34 @@ function showResult() {
         for (let [key, value] of Object.entries(parsedJSON)) {
 
             if (typeof value === 'object') {
-                resultArray.push(
-                    `<span">${key}{${Object.keys(value).length}}:</span>`
-                );
-                resultArray.push(jsonToHtml(value));
-            } else if (Array.isArray(value)) {
-                resultArray.push(`<span>${key} [${value.length}]:`);
-                resultArray.push(jsonToHtml(value));
+                const lengthStr = Array.isArray(value) ? `[${value.length}]` : `{${Object.keys(value).length}}`;
+                resultArray.push(`<span>${key} ${lengthStr}: </span>
+                                    <span class='open'>${value}</span>`);
+                resultArray.push(showResult(value));
             } else {
                 let content = value;
-
-                if (typeof content === 'number') {
-                    resultArray.push(
-                        `<p>${key}: 
-                        <span class='blue'>${content}</span>
+                const color = addClassByType(content);
+                resultArray.push(
+                    `<p>${key}: 
+                        <span class=${color}>${content}</span>
                     </p>`
-                    );
-                } else if (typeof content === 'string') {
-                    resultArray.push(
-                        `<p>${key}: 
-                        <span class='red'>${content}</span>
-                    </p>`
-                    );
-                } else {
-                    resultArray.push(
-                        `<p>${key}: 
-                        <span class='green'>${content}</span>
-                    </p>`
-                    );
-                }
+                );
             }
         }
-        return result.innerHTML = resultArray.join('');
+        return resultArray.join('');
     }
 }
 
+function renderResult(value) {
+    result.innerHTML = showResult(value);
+    openAttached();
+}
 
+function openAttached() {
+    let items = document.getElementsByClassName('open')
+
+    for (let item of items) {
+        item.addEventListener('click', () => (
+        item.classList.add('show')))
+    }
+}
