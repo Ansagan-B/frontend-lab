9b3b71b8f3d272f5ae0fee10b3a32c840ccb30da
Ansagan-B <ansagan.gb@gmail.com>
2021-08-30 22:51:12 +0300

    refactor: changeDisplay, renderResult, showResult, addClassByType, parseJSONString functions.

--- workflow/json-visualizer/script.js
+++ workflow/json-visualizer/script.js
@@ -3,7 +3,7 @@ const result = document.getElementById('result');
 const button = document.getElementById('button');
 const clearButton = document.getElementById('clear-button');
 
-button.addEventListener('click', () => renderResult(textarea.value));
+button.addEventListener('click', () => renderResult());
 clearButton.addEventListener('click', clearTextarea);
 
 function clearTextarea() {
@@ -15,79 +15,71 @@ function clearTextarea() {
 function parseJSONString(str) {
     try {
         return JSON.parse(str);
-    } catch (e) {
+    } catch (err) {
+        result.textContent = `Wrong format`;
+        result.innerHTML;
         return null;
     }
 }
 
 function addClassByType(content) {
-    if (typeof content === 'number') {
-        return 'blue';
-    } else if (typeof content === 'string') {
-        return 'red';
-    } else if (typeof content === 'boolean') {
-        return 'purple';
-    } else {
-        return 'green';
+    const colorByTypeVariants = {
+        number: 'blue',
+        string: 'red',
+        boolean: 'purple',
+        default: 'green'
     }
+    const type = typeof content;
+
+    return colorByTypeVariants[type] || colorByTypeVariants.default;
 }
 
 function showResult(val) {
     let resultArray = [];
 
-    if (val === null) {
-        result.textContent = `Wrong format`;
-        result.innerHTML;
-    } else {
-
-        for (let [key, value] of Object.entries(val)) {
-
-            if (typeof value === 'object' && value !== null) {
-                const lengthStr = Array.isArray(value) ? `[${value.length}]` : `{${Object.keys(value).length}}`;
-                resultArray.push(`<div>
-                                    <span class='clickable'>${key} ${lengthStr}: </span>
-                                    <span style='display: block' class='attached-elements'>${showResult(value)}</span>
-                                  </div>
+    for (let [key, value] of Object.entries(val)) {
+        if (typeof value === 'object' && value !== null) {
+            const lengthStr = Array.isArray(value) ? `[${value.length}]` : `{${Object.keys(value).length}}`;
+            resultArray.push(`<div>
+                                   <span class='clickable'> ${key} ${lengthStr}: </span>
+                                   <span style='display: block' class='attached-elements'> ${showResult(value)} </span>
+                                 </div>
                                   `);
-            } else {
-                let content = value;
-                const color = addClassByType(content);
-                resultArray.push(
-                    `<div><span>${key}: </span>
-                        <span class=${color}>${content}</span>
-                    </div>`
-                );
-            }
+        } else {
+            const color = addClassByType(value);
+            resultArray.push(
+                `<div>
+                     <span> ${key}: </span>
+                     <span class=${color}> ${value} </span>
+                  </div>`
+            );
         }
-        return resultArray.join('');
     }
+    return resultArray.join('');
 }
 
-function renderResult(value) {
-    const parsedJSON = parseJSONString(value);
+function renderResult() {
+    const parsedJSON = parseJSONString(textarea.value);
     result.innerHTML = showResult(parsedJSON);
     openAttached();
 }
 
-function changeDisplay(el) {
-    const nextEl = el.nextElementSibling;
+function changeDisplay(eventObj) {
+    const item = eventObj.target;
+    const nextEl = item.nextElementSibling;
 
     if (nextEl.style && nextEl.style.display === 'block') {
         nextEl.style.display = 'none';
-        el.classList.toggle('rotated');
+        item.classList.toggle('rotated');
     } else {
         nextEl.style.display = 'block';
-        el.classList.toggle('rotated');
+        item.classList.toggle('rotated');
     }
 }
 
 function openAttached() {
     let items = document.getElementsByClassName('clickable');
-
-    Array.from(items).forEach(el => {
-        el.addEventListener('click', function () {
-            changeDisplay(el)
-        });
+    Array.from(items).forEach(item => {
+        item.addEventListener('click', changeDisplay);
     })
 }
-
